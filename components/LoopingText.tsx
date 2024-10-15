b91382a3857e57d4b3c3a7565ec2633b5ff4f769
Johnny li <johnnyli@Johnnys-Laptop.local>
2024-10-15 13:18:33 -0400

    fixed show categories button bug

--- components/LoopingText.tsx
+++ components/LoopingText.tsx
@@ -12,7 +12,7 @@ const LoopingText = ({ text, direction } : Props) => {
   return (
     <Marquee direction={direction} className='flex'>
       {[...Array(6)].map((index) => (
-        <h2 key={index} className='text-[100px] md:text-[220px] opacity-40 mx-7'>{text}</h2>
+        <h2 key={index} className='text-[100px] md:text-[220px] opacity-40 mx-5'>{text}</h2>
       ))}
     </Marquee>
   )
